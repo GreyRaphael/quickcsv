e37cf6de1c223b49c764515a986b0e7bf7d7a12f
CloudGeWei <grey@pku.edu.cn>
2023-07-04 11:42:11 +0800

    finish Converter

--- src/quickcsv.h
+++ src/quickcsv.h
@@ -11,3 +11,35 @@
  *
  */
 #pragma once
+
+namespace quickcsv {
+
+template <typename T>
+struct Converter {
+    static constexpr auto convert(const std::string& str) {
+        if constexpr (std::is_same_v<T, int>)
+            return std::stoi(str);
+        else if constexpr (std::is_same_v<T, long>)
+            return std::stol(str);
+        else if constexpr (std::is_same_v<T, long long>)
+            return std::stoll(str);
+        else if constexpr (std::is_same_v<T, unsigned>)
+            return std::stoul(str);
+        else if constexpr (std::is_same_v<T, unsigned long>)
+            return std::stoul(str);
+        else if constexpr (std::is_same_v<T, unsigned long long>)
+            return std::stoull(str);
+        else if constexpr (std::is_same_v<T, char>)
+            return str[0];
+        else if constexpr (std::is_same_v<T, float>)
+            return std::stof(str);
+        else if constexpr (std::is_same_v<T, double>)
+            return std::stod(str);
+        else if constexpr (std::is_same_v<T, long double>)
+            return std::stold(str);
+        else
+            return str;
+    }
+};
+
+}  // namespace quickcsv
