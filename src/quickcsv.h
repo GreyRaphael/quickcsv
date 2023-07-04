d39abfa9d7b93a8ccf58e653953083e5003dcab5
CloudGeWei <grey@pku.edu.cn>
2023-07-04 11:23:02 +0800

    init quickcsv.h

---
+++ src/quickcsv.h
@@ -0,0 +1,13 @@
+/*
+ * quickcsv.h
+ *
+ * URL:      https://github.com/GreyRaphael/quickcsv
+ * Version:  1.0
+ *
+ * Copyright (C) 2023-2023 Wei Ge
+ * All rights reserved.
+ *
+ * quick is distributed under the BSD 3-Clause license, see LICENSE for details.
+ *
+ */
+#pragma once
