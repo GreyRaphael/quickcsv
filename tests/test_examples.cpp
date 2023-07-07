26ddaedd99e37154f65cce5bd8730e4dbf4903eb
CloudGeWei <grey@pku.edu.cn>
2023-07-08 00:07:10 +0800

    execute test examples

---
+++ tests/test_examples.cpp
@@ -0,0 +1,11 @@
+#include "example1.h"
+#include "example2.h"
+#include "example3.h"
+#include "example4.h"
+
+int main() {
+    test_example1("test.csv");
+    test_example2("test.csv");
+    test_example3("test.csv");
+    test_example4("test.csv");
+}
