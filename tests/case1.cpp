cd88e4dc5710c9f024dbfa64e1ab20d3b606ef08
CloudGeWei <grey@pku.edu.cn>
2023-07-07 14:09:37 +0800

    finish test exaples

--- tests/case1.cpp
+++ tests/case1.cpp
@@ -8,8 +8,8 @@ void test_converter(std::string const& str) {
     std::cout << typeid(val).name() << '\t' << val << '\n';
 }
 
-void test_read_csv(std::string const& filename) {
-    auto table = quickcsv::read_csv(filename);
+void test_read_raw_csv(std::string const& filename) {
+    auto table = quickcsv::read_raw_csv(filename);
 
     for (auto&& row : table) {
         for (auto&& cell : row) {
@@ -19,9 +19,22 @@ void test_read_csv(std::string const& filename) {
     }
 }
 
+void test_read_csv(std::string const& filename) {
+    quickcsv::Param parameters{
+        .sep = ',',
+        .skiprows = 0,
+        .usecols = {"symbol", "close", "volume"},
+    };
+    auto [symbol_vec, close_vec, volume_vec] = quickcsv::read_csv<std::string, double, int>(filename, parameters);
+    for (unsigned i = 0; i < symbol_vec.size(); ++i) {
+        std::cout << symbol_vec[i] << '\t' << close_vec[i] << '\t' << volume_vec[i] << '\n';
+    }
+}
+
 int main() {
     // test_converter<int>("12345");
     // test_converter<double>("12346.1");
     // test_converter<std::string>("12346.1");
+    // test_read_raw_csv("test.csv");
     test_read_csv("test.csv");
 }
